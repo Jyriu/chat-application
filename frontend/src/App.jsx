8c403d1544768568277223cfd6bd4af88610f69a
Jyriu <sami.yezza@gmail.com>
2024-03-24 15:38:33 +0100

    frontend start

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,13 @@
 import './App.css'
+import SignUp from '/src/pages/signup/SignUp'
+import Login from '/src/pages/login/Login'
 
 function App() {
 
   return (
-    <>
-     <button className="btn">Button</button>
-<button className="btn btn-neutral">Neutral</button>
-<button className="btn btn-primary">Primary</button>
-<button className="btn btn-secondary">Secondary</button>
-<button className="btn btn-accent">Accent</button>
-<button className="btn btn-ghost">Ghost</button>
-<button className="btn btn-link">Link</button>
-    </>
+    <div className='p-4 h-screen flex items-venter justify-center'>
+      <SignUp />
+    </div>
   )
 }
 
---
+++ frontend/src/pages/login/Login.jsx
@@ -0,0 +1,82 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div className='flex flex-col items-venter justify-center min-w-96 mx-auto'>
+        <div className='w-full p-4 bg-gray-800 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-0 border border-gray-100'>
+            <h1 className='text-3xl font-semibold text-center text-gray-300'>
+            Se connecter
+                <span className='text-blue-500'> talkz</span>
+            </h1>
+
+            <form>
+                <div>
+                    <label className='label p-2'>
+                        <span className='text-base label-text'>Nom d'utilisateur</span>
+                    </label>
+                    <input type='text' placeholder="Nom d'utilisateur" className='w-full input input-bordered h-10' />
+                </div>
+
+                <div>
+                    <label className='label'>
+                        <span className='text-base label-text'>Mot de passe</span>
+                    </label>
+                    <input type='password' placeholder="Mot de passe" className='w-full input input-bordered h-10' />
+                </div>
+
+                <a href='#' className='text-sm hover:underline hover:text-blue-500 mt-2 inline-block'>
+                    {"Vous"} n'avez pas de compte ?
+                </a>
+
+                <div>
+                    <button className='btn btn-block btn-sm mt-2'>Se connecter</button>
+                </div>
+            </form>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Login
+
+// STARTER CODE FOR THIS FILE
+// const Login = () => {
+//     return (
+//       <div className='flex flex-col items-venter justify-center min-w-96 mx-auto'>
+//           <div className='w-full p-4 bg-gray-800 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-0 border border-gray-100'>
+//               <h1 className='text-3xl font-semibold text-center text-gray-300'>
+//               Se connecter
+//                   <span className='text-blue-500'> talkz</span>
+//               </h1>
+  
+//               <form>
+//                   <div>
+//                       <label className='label p-2'>
+//                           <span className='text-base label-text'>Nom d'utilisateur</span>
+//                       </label>
+//                       <input type='text' placeholder="Nom d'utilisateur" className='w-full input input-bordered h-10' />
+//                   </div>
+  
+//                   <div>
+//                       <label className='label'>
+//                           <span className='text-base label-text'>Mot de passe</span>
+//                       </label>
+//                       <input type='password' placeholder="Mot de passe" className='w-full input input-bordered h-10' />
+//                   </div>
+  
+//                   <a href='#' className='text-sm hover:underline hover:text-blue-500 mt-2 inline-block'>
+//                       {"Vous"} n'avez pas de compte ?
+//                   </a>
+  
+//                   <div>
+//                       <button className='btn btn-block btn-sm mt-2'>Se connecter</button>
+//                   </div>
+//               </form>
+//           </div>
+        
+//       </div>
+//     )
+//   }
+  
+//   export default Login
